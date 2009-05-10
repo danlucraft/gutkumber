8bc528cb3320fa4bbfcfbcdac61b4bbca0986c11
Daniel Lucraft <dan@fluentradical.com>
2009-05-10 08:47:12 +0100

    Added application startup helpers

--- lib/gutkumber.rb
+++ lib/gutkumber.rb
@@ -10,3 +10,27 @@ require 'gutkumber/gtk/dialog'
 
 Dir[File.dirname(__FILE__) + "/gutkumber/steps/*_steps.rb"].each {|fn| require fn }
 
+module Gutkumber
+  def self.in_test_process?
+    true
+  end
+  
+  def self.begin_tests
+    @ready_to_test = true
+  end
+  
+  def self.ready_to_test?
+    @ready_to_test
+  end
+  
+  def self.start_application_thread(&block)
+    Thread.new do
+      block.call
+    end
+    
+    loop do
+      sleep 0.1
+      break if Gutkumber.ready_to_test?
+    end
+  end
+end
